cce79350ce92c1f43240b6b723e7b31cf63c6d01
Mohaiminul-Islam-AppzoneIT <mohaiminul.islam@appzoneit.com>
2024-11-06 21:42:03 +0600

    Fix the errors and make the migrations

--- marketplace/models.py
+++ marketplace/models.py
@@ -6,7 +6,7 @@ from menu.models import Product
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, FoodItem
+
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('category_name',)}
@@ -7,12 +8,12 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('category_name', 'vendor__vendor_name')
 
 
-class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('product_name',)}
-    list_display = ('product_name', 'category', 'vendor', 'price', 'is_available', 'updated_at')
-    search_fields = ('product_name', 'category__category_name', 'vendor__vendor_name', 'price')
+class FoodItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('food_title',)}
+    list_display = ('food_title', 'category', 'vendor', 'price', 'is_available', 'updated_at')
+    search_fields = ('food_title', 'category__category_name', 'vendor__vendor_name', 'price')
     list_filter = ('is_available',)
 
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
