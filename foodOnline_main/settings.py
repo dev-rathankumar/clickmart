4fdba74a13acc691709dc818d72d18be0d38237d
Rathan Kumar <rathan@Rathans-MacBook-Pro.local>
2024-12-19 13:07:00 +0530

    deployment started

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['172.105.253.185', '127.0.0.1', 'clickmall.in', 'www.clickmall.in']
 
 
 # Application definition
